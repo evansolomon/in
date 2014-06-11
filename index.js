bc52f553b4549419cefe608962a822e2d146aa3c
Evan Solomon <evan@evanalyze.com>
2014-06-10 22:06:56 -0700

    first

---
+++ index.js
@@ -0,0 +1,41 @@
+var child_process = require('child_process')
+
+module.exports = function (args, callback) {
+  switch (args[0]) {
+    // Help output
+    case undefined:
+    case 'help':
+    case '--help':
+    case '-h':
+      callback(null, [
+        'Usage:',
+        '  q [MINUTES] [MESSAGE]',
+        '',
+        'Example:',
+        '  q 10 put the milk back in the fridge'
+      ].join('\n'))
+      break
+
+    // List existing jobs
+    case 'list':
+    case '--list':
+      child_process.exec('ps aux | grep -e "# queued message$" | grep -v grep', function (err, out) {
+        // Errors just mean no jobs were found, ignore them
+        callback(null, out)
+      })
+      break
+
+    // Create a new job
+    default:
+      var delay = parseInt(args.shift(), 10) * 60
+      var sleep = 'sleep ' + delay + '; '
+
+      var message = args.join(' ')
+      var notify = 'osascript -e \'display notification "' + message + '" with Title "Reminder"\''
+
+      var command = sleep + notify + ' # queued message'
+
+      child_process.exec(command).unref()
+      callback()
+  }
+}
